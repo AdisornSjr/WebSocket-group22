cef72b666a59d90d32b0dd10b83a361a0934986a
Fluke <adisorn.sjr@gmail.com>
2024-02-19 15:09:19 +0700

    addUser

--- SpringSocketDemo/src/main/java/com/websocket/demo/chat/chatController.java
+++ SpringSocketDemo/src/main/java/com/websocket/demo/chat/chatController.java
@@ -1,13 +1,16 @@
 package com.websocket.demo.chat;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class chatController {
-
+    private final SimpMessageSendingOperations messageSendingOperations;
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(ChatMessage chatMessage) {
@@ -18,6 +21,9 @@ public class chatController {
     @SendTo("/topic/public")
     public ChatMessage addUser(ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        chatMessage.updateCount(1); //why it suggest
+        chatMessage.setCount();
+        System.out.println(chatMessage.getCount());
         return chatMessage;
     }
 }
